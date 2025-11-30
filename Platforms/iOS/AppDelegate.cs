8eb1b4f20472ed63c639bc8ba59590384fe462ab
Ofir Gavish <gavishofir@microsoft.com>
2025-11-30 23:14:48 +0200

    Simplify: Remove old SDK (Xamarin-only), add global.json for .NET 8

--- Platforms/iOS/AppDelegate.cs
+++ Platforms/iOS/AppDelegate.cs
@@ -1,23 +1,9 @@
 ﻿using Foundation;
-#if IOS
-using Microsoft.Intune.MAM;
-#endif
 
 namespace AzureDeploymentsApp;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-#if IOS
-	public override bool FinishedLaunching(UIKit.UIApplication application, NSDictionary launchOptions)
-	{
-		// Initialize OLD Intune SDK (version 18.x)
-		// This simulates a customer's legacy app that needs wrapping with new wrapper
-		IntuneMAMEnrollmentManager.Instance.LoginAndEnrollAccount(null);
-		
-		return base.FinishedLaunching(application, launchOptions);
-	}
-#endif
-
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
